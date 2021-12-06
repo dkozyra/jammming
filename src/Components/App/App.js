50999b443afea53970c163ef73731397595a57bc
Damian Kozyra <d.kozyra@hotmail.co.uk>
2021-12-06 22:19:02 +0000

    passing props

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,23 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {name: 'name1', artist: 'artist1', album: 'album1', id: 1},
+        {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
+        {name: 'name3', artist: 'artist3', album: 'album3', id: 3}
+      ],
+      playlistTracks: [
+        {name: 'nameA', artist: 'artistA', album: 'albumA', id: 1},
+        {name: 'nameB', artist: 'artistB', album: 'albumB', id: 2},
+        {name: 'nameC', artist: 'artistC', album: 'albumC', id: 3}
+      ],
+      playlistName: 'New playlist'
+    }
+  }
+
   render() {
     return (
       <div>
@@ -12,8 +29,8 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
-            <Playlist />
+            <SearchResults searchResults={this.state.searchResults}/>
+            <Playlist playlistTracks={this.state.playlistTracks} playlistName={this.state.playlistName}/>
           </div>
         </div>
       </div>
