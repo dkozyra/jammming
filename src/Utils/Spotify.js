1585e3539c6fdc6d41f06b2fbacce73d31ffd608
Damian Kozyra <d.kozyra@hotmail.co.uk>
2021-12-07 19:13:33 +0000

    spotify access token complete

--- src/Utils/Spotify.js
+++ src/Utils/Spotify.js
@@ -1,7 +1,29 @@
-const token = '';
+let accessToken;
+let expires;
+let clientId = 'ff075deed9584b37bad2f720511c45de';
+let redirectURI = 'http://localhost:3000/';
 
-class Spotify {
+const Spotify = {
 
+    getAccessToken() {
+        if(accessToken) {
+            return accessToken;
+        }
+
+        const accessTokenURL = window.location.href.match(/access_token=([^&]*)/);
+        const expiresURL = window.location.href.match(/expires_in=([^&]*)/);
+
+        if(accessTokenURL && expiresURL) {
+            accessToken = accessTokenURL[1];
+            expires = Number(expiresURL[1]);
+            window.setTimeout(() => accessToken = '', expires * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        } else {
+            const accessURL = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+            window.location = accessURL;
+        }
+    }
 };
 
 
